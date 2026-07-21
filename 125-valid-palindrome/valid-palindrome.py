26500e5fa133b1a9e6953969d58d35b70f14de91
Yashansh Gupta <yashanshgupta@gmail.com>
2026-07-21 14:28:12 +0530

    Time: 15 ms (72.62%) | Memory: 14.9 MB (5.58%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        q=[]
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                if i.isalpha():
+                    i=i.lower()
+                q.append(i)
+        
+        if q==q[::-1]:
+            return True
+        else:
+            return False
