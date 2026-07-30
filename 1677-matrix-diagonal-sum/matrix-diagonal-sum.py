668e8ded41bc4071aaf5f315d40b6b1359e2d77f
Yashansh Gupta <yashanshgupta@gmail.com>
2026-07-30 20:04:30 +0530

    Time: 5 ms (9.17%) | Memory: 12.5 MB (77.22%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -9,10 +9,9 @@ class Solution(object):
 
         s=0
         for i in range(row):
+            s+=mat[i][i]
             for j in range(col):
-                if i==j:
-                    s+=mat[i][j]
-                elif i+j==col-1:
+                if i+j==col-1 and i!=j:
                     s+=mat[i][j]
 
         return s
