8d4ee4c2eeb0e2437d5b9f2a7470aa1a6b75de4b
Yashansh Gupta <yashanshgupta@gmail.com>
2026-08-02 17:52:39 +0530

    Time: 55 ms (42.32%) | Memory: 12.6 MB (71.06%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -4,21 +4,23 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        rows = len(grid)
-        cols = len(grid[0])
+        row=len(grid)
+        col=len(grid[0])
 
-        perimeter = 0
+        c=0
 
-        for r in range(rows):
-            for c in range(cols):
-
-                if grid[r][c] == 1:
-                    perimeter += 4
-
-                    if r + 1 < rows and grid[r + 1][c] == 1:
-                        perimeter -= 2
-
-                    if c + 1 < cols and grid[r][c + 1] == 1:
-                        perimeter -= 2
-
-        return perimeter
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]==1:
+                    s=4
+                    q=0
+                    if i>0 and grid[i-1][j]==1:
+                        q+=1
+                    if i<row-1 and grid[i+1][j]==1:
+                        q+=1
+                    if j>0 and grid[i][j-1]==1:
+                        q+=1
+                    if j<col-1 and grid[i][j+1]==1:
+                        q+=1
+                    c+=s-q
+        return c
