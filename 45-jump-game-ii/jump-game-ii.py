d272a608a98829b270a08931a96ca8b7bed13b7b
Yashansh Gupta <yashanshgupta@gmail.com>
2026-09-05 17:03:17 +0530

    Time: 4 ms (69.64%) | Memory: 20.1 MB (65.89%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l = 0
+        r = 0
+        c = 0
+
+        while r < len(nums) - 1:
+            farthest = r
+
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            l = r + 1
+            r = farthest
+            c += 1
+
+        return c
+
+
+        
