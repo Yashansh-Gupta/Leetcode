d814fe274e186326da9bf248e380495c677988dd
Yashansh Gupta <yashanshgupta@gmail.com>
2026-09-24 13:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.77%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,10 +1,14 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits) - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
+    def plusOne(self, digits: list[int]) -> list[int]:
+        q=''.join(map(str,digits))
+        p=int(q)
+        p+=1
+        l=[]
+        o=str(p)
+        for i in range(len(o)):
+            q=int(o[i])
+            l.append(q)
 
-        digits.insert(0, 1)
-        return digits
+        return l
+
+        
